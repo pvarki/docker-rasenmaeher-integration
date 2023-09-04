f838baee2a766730db8c6f87eea66a6f1f5120bd
aarne33 <126289237+aarne33@users.noreply.github.com>
2023-09-04 20:08:45 +0300

    Fixed test status testcase names

--- tests/test_status.py
+++ tests/test_status.py
@@ -2,14 +2,14 @@
 
 import requests
 
-def test_get_dog_init_status(localmaeher_api, testdata):
+def test_get_init_status(localmaeher_api, testdata):
     url = f"{localmaeher_api[0]}/{localmaeher_api[1]}/enrollment/status/{testdata['work_id1']}"
     response = requests.get(url, json=None, headers=None, verify=False)
     print(response.json())
     assert response.status_code == 200
     assert response.json()["status"] == "init"
 
-def test_set_dog_new_state(localmaeher_api, testdata):
+def test_set_new_state(localmaeher_api, testdata):
     url = f"{localmaeher_api[0]}/{localmaeher_api[1]}/enrollment/config/set-state"
     data = {
         "state":"new", 
@@ -22,14 +22,14 @@ def test_set_dog_new_state(localmaeher_api, testdata):
     assert response.status_code == 200
     assert response.json()["success"] is True
 
-def test_get_dog_new_status(localmaeher_api, testdata):
+def test_get_new_status(localmaeher_api, testdata):
     url = f"{localmaeher_api[0]}/{localmaeher_api[1]}/enrollment/status/{testdata['work_id1']}"
     response = requests.get(url, json=None, headers=None, verify=False)
     print(response.json())
     assert response.status_code == 200
     assert response.json()["status"] == "new"
 
-def test_set_dog_init_state(localmaeher_api, testdata):
+def test_set_init_state(localmaeher_api, testdata):
     url = f"{localmaeher_api[0]}/{localmaeher_api[1]}/enrollment/config/set-state"
     data = {
         "state":"init", 
