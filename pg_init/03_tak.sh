29f9f14a89b2b91a52e5a905a64dabd7124b79f8
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-10-14 23:53:36 +0300

    Create takserver containers, the db init must be run as superuser due to schemamanager being stupid so we have to do tricks to get the normal tak user access to the tables schemamanager creates

--- pg_init/03_tak.sh
+++ pg_init/03_tak.sh
@@ -12,3 +12,33 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE tak;
     GRANT ALL PRIVILEGES ON DATABASE tak TO tak;
 EOSQL
+# Adjust default privileges and make a trigger to reassign table ownerships so the normal tak user can use tables
+# We need superuser privileges to run the SchemaManager and without this the tables will be owned by the superuser
+# and tak normal user has no access :(
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "tak" <<-EOSQL
+    CREATE OR REPLACE FUNCTION rm_func_create_set_owner()
+     RETURNS event_trigger
+     LANGUAGE plpgsql
+    AS \$function\$
+    DECLARE
+        obj record;
+    BEGIN
+        FOR obj IN
+            SELECT *
+            FROM pg_event_trigger_ddl_commands()
+            WHERE command_tag='CREATE TABLE'
+        LOOP
+            if obj.object_identity ~ '.*'
+            THEN
+                EXECUTE format('ALTER TABLE %s OWNER TO tak', obj.object_identity);
+            end if;
+        END LOOP;
+    END;
+    \$function\$;
+    CREATE EVENT TRIGGER rm_trg_create_set_owner
+        ON ddl_command_end
+        WHEN tag IN ('CREATE TABLE')
+        EXECUTE PROCEDURE rm_func_create_set_owner();
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO  tak;
+EOSQL
+# Sadly schemamanager nukes all procedures and triggers as the first thing it does when it sees a db without migration state
