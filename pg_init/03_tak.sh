cadc44e5c9242a9f23f9967ea09821f43bb6a869
Eero af Heurlin <eero.afheurlin@iki.fi>
2023-10-14 18:44:01 +0300

    create db and user for tak (and solve some FIXMEs while adding some more)

--- pg_init/03_tak.sh
+++ pg_init/03_tak.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-if [ -z "$KEYCLOAK_PASSWORD" ]
+if [ -z "TAK_PASSWORD" ]
 then
-  echo "KEYCLOAK_PASSWORD not set"
-  exit 1
+  echo "TAK_PASSWORD not set, not creating the DB"
+  exit 0
 fi
 set -e
 # FIXME: Can we use mTLS for auth ??
 # FIXME: Get the user and db name from ENV too (or things break when someone changes the defaults)
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER keycloak WITH ENCRYPTED PASSWORD '$KEYCLOAK_PASSWORD';
-    CREATE DATABASE keycloak;
-    GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+    CREATE USER tak WITH ENCRYPTED PASSWORD '$TAK_PASSWORD';
+    CREATE DATABASE tak;
+    GRANT ALL PRIVILEGES ON DATABASE tak TO tak;
 EOSQL
