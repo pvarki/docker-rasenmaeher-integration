171d115f73bca4711851f728398fca5364f2975c
Eero af Heurlin <eero.afheurlin@iki.fi>
2026-06-17 20:13:55 +0300

    feat: If we found pvarki images in docker hub copy them to ghcr.io as well (should only happen for tak-server-dist and not even that once we get everything set up perfectly)

--- .github/retag_push_ext_images.sh
+++ .github/retag_push_ext_images.sh
@@ -11,10 +11,17 @@ then
 fi
 export HUB_DOCKER_REPO=${ORIG_REPO}
 # shellcheck disable=SC2086
-for IMGNAME in $(docker compose -f docker-compose-local.yaml ${EXTRA_ARGS} config --format json | jq -r '.services[].image' | sort | uniq | grep "${ORIG_REPO}")
+for IMGNAME in $(docker compose -f docker-compose-local.yml ${EXTRA_ARGS} config --format json | jq -r '.services[].image' | sort | uniq | grep "${ORIG_REPO}")
 do
   # shellcheck disable=SC2001
   NEWNAME=$(echo "${IMGNAME}" | sed -e s%"${ORIG_REPO}"%"${ACR_REPO}/"%g)
   echo "buildx imagetools create --tag ${NEWNAME} ${IMGNAME}"
   docker buildx imagetools create --tag "${NEWNAME}" "${IMGNAME}"
+  # If we got pvarki image from docker hub copy it to ghcr.io as well
+  if grep -q '/pvarki/' <<<"$IMGNAME"; then
+    # shellcheck disable=SC2001
+    GHCRNAME=$(echo "${IMGNAME}" | sed -e s%"${ORIG_REPO}"%"ghcr.io/"%g)
+    echo "buildx imagetools create --tag ${GHCRNAME} ${IMGNAME}"
+    docker buildx imagetools create --tag "${GHCRNAME}" "${IMGNAME}"
+  fi
 done
